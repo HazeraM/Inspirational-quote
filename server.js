844db03afb6f3094c87ba732fdb0f9bd02b06796
Hazera Munim <hazeramunim@Hazeras-MacBook-Air.local>
2020-02-14 16:34:22 -0500

    created functions to handle requests

---
+++ server.js
@@ -0,0 +1,17 @@
+// require the built-in the HTTP node module (no need to npm install)
+var http = require("http");
+
+// define the ports we want to listen to
+var PORTONE = 8000;
+var PORTTWO = 8080;
+
+
+// functions to handle requests, one for each server
+
+function handleRequestOne(request, response) {
+    response.end("Always belive something wonderful is about to happen.");
+}
+
+function handleRequestTwo(request, response) {
+    response.end("Never trust a computer you can't throw out a window.");
+}
